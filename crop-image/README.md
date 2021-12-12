1f8dc9236d07dcb1ee81b24e39f0b0f7a94b1819
I Dewa Putu Ardi Nusawan <ardi.nusawan13@gmail.com>
2021-12-12 17:11:47 +0800

    Support bulk

--- crop-image/README.md
+++ crop-image/README.md
@@ -1,14 +1,38 @@
 # Crop
 
 ## Prerequire
-Install dependencies
+Use virtualenv and install dependencies needed
 ```sh
-virtualenv ven
+virtualenv venv
 source venv/bin/activate
 pip install -r requirements.txt
 ```
 
 ## Usage
-```python
-python crop.py -w 500 -h 692 -i sample.png
-```
+It detected file mode from args width and height and only process image with same mode.
+
+1. One file
+    - Potrait
+    ```python
+    python crop.py -w 500 -h 692 -i sample_potrait.png
+    ```
+    - Landscape
+    ```python
+    python crop.py -w 800 -h 539 -i sample_landscape.jpeg
+    ```
+
+1. Bulk
+    - Potrait
+    ```python
+    python crop.py -w 500 -h 692 -p "."
+    ```
+    - Landscape
+    ```python
+    python crop.py -w 800 -h 539 -p "."
+    ```
+
+New file will exist with suffix `_thumbnail`
+
+### Links
+- https://www.holisticseo.digital/python-seo/resize-image/
+- https://www.tutorialspoint.com/python_pillow/python_pillow_creating_thumbnails.htm
--- crop-image/crop.py
+++ crop-image/crop.py
@@ -4,49 +4,85 @@ import os
 import sys, getopt
 from PIL import Image
 
-new_height = ''
-new_width = ''
-file_name = ''
+# Crop excess width or height
+def crop(image, new_width, new_height):
+    width, height = image.size
+    left = (width - new_width)/2
+    top = (height - new_height)/2
+    right = (width + new_width)/2
+    bottom = (height + new_height)/2
 
-###############################
-# o == option
-# a == argument passed to the o
-###############################
-# Cache an error with try..except
-# Note: options is the string of option letters that the script wants to recognize, with
-# options that require an argument followed by a colon (':') i.e. -h new_height
-try:
-    myopts, args = getopt.getopt(sys.argv[1:],"h:w:i:")
-except getopt.GetoptError as e:
-    print (str(e))
-    print("Usage: %s -w new_width -h new_height -i input_file " % sys.argv[0])
-    sys.exit(2)
+    # Crop the center of the image
+    cropped_image = image.crop((left, top, right, bottom))
+    return cropped_image
 
-for o, a in myopts:
-    if o == '-h':
-        new_height=int(a)
-    elif o == '-w':
-        new_width=int(a)
-    elif o == '-i':
-        file_name=a
+# Save to new file
+def save(image, file_name):
+    # extract the file name and extension
+    split_tup = os.path.splitext(file_name)
+    file_name = split_tup[0]
+    file_extension = split_tup[1]
+    final_name = file_name + '_thumbnail' + file_extension
+    image.save(final_name)
 
-# PIL
-im = Image.open(file_name)
-width, height = im.size
+# Resize and crop image. Only process if mode of args given and exisiting image is same. I.e if args is potrait, only process potrait image
+def process(img, new_width, new_height):
+    image = Image.open(img)
+    horizontal=image.size[0] > image.size[1] and new_width > new_height
+    vertical=image.size[0] < image.size[1] and new_width < new_height
 
-left = (width - new_width)/2
-top = (height - new_height)/2
-right = (width + new_width)/2
-bottom = (height + new_height)/2
+    if horizontal:
+        image.thumbnail(size=(image.size[0], new_height), resample=Image.NEAREST)
+        image = crop(image, new_width, new_height)
+        save(image, img)
+    elif vertical:
+        image.thumbnail(size=(new_width, image.size[1]), resample=Image.NEAREST)
+        image = crop(image, new_width, new_height)
+        save(image, img)
+    else:
+        msg = "Info: skip {file_name} because different mode"
+        print(msg.format(file_name=img))
 
-# Crop the center of the image
-im = im.crop((left, top, right, bottom))
+def main():
+    new_height = ''
+    new_width = ''
+    file_name = ''
+    path = ''
 
-# save file
-split_tup = os.path.splitext(file_name)
+    ###############################
+    # o == option
+    # a == argument passed to the o
+    ###############################
+    # Cache an error with try..except
+    # Note: options is the string of option letters that the script wants to recognize, with
+    # options that require an argument followed by a colon (':') i.e. -h new_height
+    try:
+        myopts, args = getopt.getopt(sys.argv[1:],"h:w:i:p:")
+    except getopt.GetoptError as e:
+        print (str(e))
+        print("Usage: %s -w new_width -h new_height -i input_file -p path_folder" % sys.argv[0])
+        sys.exit(2)
 
-# extract the file name and extension
-file_name = split_tup[0]
-file_extension = split_tup[1]
-final_name = file_name + '-cropped' + file_extension
-im.save(final_name)
+    for o, a in myopts:
+        if o == '-h':
+            new_height=int(a)
+        elif o == '-w':
+            new_width=int(a)
+        elif o == '-i':
+            file_name=a
+        elif o == '-p':
+            path=a
+
+    if len(path) > 0:
+        # bulk
+        images = [file for file in os.listdir(path) if file.endswith(('jpeg', 'png', 'jpg'))]
+        for img in images:
+            process(img, new_width, new_height)
+    elif len(file_name) > 0:
+        # one file
+        process(file_name, new_width, new_height)
+    else:
+        print("Pass")
+
+if __name__=="__main__":
+    main()
